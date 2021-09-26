6ddd61207924a6e3f06352c32505c21117be7f91
一只不願透露姓名的小学鷄 <20664431+forever765@users.noreply.github.com>
2021-09-26 19:29:23 +0800

    add global page loading bar on header, via nprogress

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import 'element-plus/dist/index.css'
 import './style/element_visiable.scss'
 import ElementPlus from 'element-plus'
 import zhCn from 'element-plus/es/locale/lang/zh-cn'
+// 引入 NProgress显示页面加载进度条
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
 // 引入gin-vue-admin前端初始化相关内容
 import './core/gin-vue-admin'
 // 引入封装的router
@@ -14,6 +17,24 @@ import '@/permission'
 import { store } from '@/store/index'
 
 import App from './App.vue'
+
+NProgress.configure({
+  easing: 'ease',
+  speed: 500,
+  showSpinner: true,
+  trickleSpeed: 100,
+  minimum: 0.3
+})
+
+router.beforeEach((to, from, next) => {
+  NProgress.start()
+  next()
+})
+
+router.afterEach(() => {
+  NProgress.done()
+})
+
 const app = createApp(App)
 app.config.productionTip = false
 app.use(run)
