88ff6ea8f109ef5976f301a4538ac2bad8bd5cd7
forever0765 <9353408+forever0765@user.noreply.gitee.com>
2021-11-10 17:08:44 +0800

    update index chart select sql, due to kafka upgraded to cluster

--- server/utils/charts_traffic.go
+++ server/utils/charts_traffic.go
@@ -27,14 +27,26 @@ func GetTraffic() json.RawMessage {
 	sqlDB, err := db.DB()
 	sqlDB.SetMaxIdleConns(2)
 	sqlDB.SetMaxOpenConns(100)
-	// RAW SQL: SELECT In.Time,In.InTrafficMbps,Out.OutTrafficMbps FROM (SELECT Time,InTrafficMbps FROM `InTraffic` WHERE Time > '2021-10-06 16:05:31') as In, (SELECT Time,OutTrafficMbps FROM `OutTraffic` WHERE Time > '2021-10-06 16:05:31') as Out WHERE In.Time = Out.Time
 	now := time.Now()
 	h, _ := time.ParseDuration("-1h")
 	anHourAgo := (now.Add(h)).Format("2006-01-02 15:04:05")
 	var result []Traffic
-	subQuery1 := db.Table("InTraffic").Select("Time,InTrafficMbps").Where("Time > ?", anHourAgo)
-	subQuery2 := db.Table("OutTraffic").Select("Time,OutTrafficMbps").Where("Time > ?", anHourAgo)
-	db.Table("(?) as In, (?) as Out", subQuery1, subQuery2).Select("Time,FLOOR(InTrafficMbps,2) as in_traffic_mbps,FLOOR(OutTrafficMbps,2) as out_traffic_mbps").Where("In.Time = Out.Time ORDER BY Time").Find(&result)
+	// RAW SQL: SELECT In.Time,In.InTrafficMbps,Out.OutTrafficMbps FROM (SELECT Time,InTrafficMbps FROM `InTraffic` WHERE Time > '2021-10-06 16:05:31') as In, (SELECT Time,OutTrafficMbps FROM `OutTraffic` WHERE Time > '2021-10-06 16:05:31') as Out WHERE In.Time = Out.Time
+	//subQuery1 := db.Table("InTraffic").Select("Time,InTrafficMbps").Where("Time > ?", anHourAgo)
+	//subQuery2 := db.Table("OutTraffic").Select("Time,OutTrafficMbps").Where("Time > ?", anHourAgo)
+	//db.Table("(?) as In, (?) as Out", subQuery1, subQuery2).Select("Time,FLOOR(InTrafficMbps,2) as in_traffic_mbps,FLOOR(OutTrafficMbps,2) as out_traffic_mbps").Where("In.Time = Out.Time ORDER BY Time").Find(&result)
+
+	// RAW SQL: select In.Time,In.InTrafficMbps,Out.OutTrafficMbps FROM(
+	//select toStartOfInterval(timestamp_max, INTERVAL 10 second ) as Time, sum(bytes)/1048576 as InMBytes, sum(packets) as InPackets, sum(bytes)/838860.8 as InTrafficMbps
+	//from(select * from nms_data.pmacctd_data prewhere timestamp_max >= NOW() - 3600) where loc_dst = '局域网'
+	//group by Time
+	//order by Time) as In,(select toStartOfInterval(timestamp_max, INTERVAL 10 second ) as Time, sum(bytes)/1048576 as OutMBytes, sum(packets) as OutPackets, sum(bytes)/838860.8 as OutTrafficMbps
+	//from(select * from nms_data.pmacctd_data prewhere timestamp_max >= NOW() - 3600) where loc_src = '局域网'
+	//group by Time
+	//order by Time) as Out WHERE In.Time=Out.Time;
+	subQuery1 := db.Raw("select toStartOfInterval(timestamp_max, INTERVAL 10 second ) as Time, sum(bytes)/1048576 as InMBytes, sum(packets) as InPackets, sum(bytes)/838860.8 as InTrafficMbps from(select * from nms_data.pmacctd_data prewhere timestamp_max >= ?) where loc_dst = '局域网' group by Time order by Time", anHourAgo)
+	subQuery2 := db.Raw("select toStartOfInterval(timestamp_max, INTERVAL 10 second ) as Time, sum(bytes)/1048576 as OutMBytes, sum(packets) as OutPackets, sum(bytes)/838860.8 as OutTrafficMbps from(select * from nms_data.pmacctd_data prewhere timestamp_max >= ?) where loc_src = '局域网' group by Time order by Time", anHourAgo)
+	db.Table("(?) as In, (?) as Out", subQuery1, subQuery2).Select("In.Time,FLOOR(In.InTrafficMbps,2) as in_traffic_mbps,FLOOR(Out.OutTrafficMbps,2) as out_traffic_mbps").Where("In.Time = Out.Time ORDER BY Time").Find(&result)
 	result2, _ := json.Marshal(result)
 	return json.RawMessage(result2)
 }
